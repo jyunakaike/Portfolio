e78b353cfe5eb231ffa8b9ca8aed9ac49292c158
jyun <jyunakaike@gmail.com>
2022-02-14 10:21:53 -0400

    feat: Header style basic

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './style.css';
 
-
-
 export const Header = () => {
     return (
         <div className='Header-container' >
             <header >
-                <span>Jyunik</span>
-                <span>Menu</span>
+                <span className='Header-Logo'>Jyunik</span>
+                <div className='Header-Categories'>
+                    <h2>Section1</h2>
+                    <h2>Section2</h2>
+                    <h2>Section3</h2>
+                </div>
+
             </header>
         </div>
     )
