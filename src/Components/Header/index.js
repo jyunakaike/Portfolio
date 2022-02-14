b5242b42f5fd985665f9d1f7c5269eef0cd02e5a
jyun <jyunakaike@gmail.com>
2022-02-14 13:12:08 -0400

    feat: header scroll

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './style.css';
 
 export const Header = () => {
-    return (
-        <div className='Header-container' >
-            <header >
-                <span className='Header-Logo'>Jyunik</span>
-                <div className='Header-Categories'>
-                    <h2>Section1</h2>
-                    <h2>Section2</h2>
-                    <h2>Section3</h2>
-                </div>
+    const [showFixed, setShowFixed] = useState(false)
 
-            </header>
-        </div>
+    useEffect(() => {
+      const onScroll =() => {
+          const newShowFixed = window.scrollY > 100
+          showFixed !== newShowFixed && setShowFixed(newShowFixed)
+      }
+
+      document.addEventListener('scroll', onScroll)
+    
+      return () => {
+          document.removeEventListener('scroll', onScroll)
+      }
+    }, [showFixed])
+
+    return (
+        <header className={` ${(showFixed) ? "header-small" : "header"}`} >
+            <div className={` ${(showFixed) ? "small" : "Header-container"}`}>
+                 <span className='Header-Logo'>Jyunik</span>
+                 <div className='Header-Categories'>
+                     <h2>Section1</h2>
+                     <h2>Section2</h2>
+                     <h2>Section3</h2>
+                 </div>
+
+             </div>
+        </header>
     )
 }
