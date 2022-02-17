4c98730accb582a8fac05657d92369e76176949c
jyun <jyunakaike@gmail.com>
2022-02-17 19:49:01 -0400

    fix: About me adding space around

--- src/Components/Me/index.js
+++ src/Components/Me/index.js
@@ -13,28 +13,34 @@ export const Me = ({ refer }) => {
     return (
         <section ref={myRef} className='About-container'>
             <div className='About-detail'>
-                <h2>About Me</h2>
-                <p>
-                    Hi! I'm Jorge Jyun Nakamura Ikeda, born in 1994.<br />
-                    I'm enthusiastic about Learning tools, skills and new technologies. Now my main focus is learn and earn new skills and Experience of frontend Developer.
-                </p>
-                <h3>Skills</h3>
+                <div>
+                    <h2>About Me</h2>
+                    <p>
+                        Hi! I'm Jorge Jyun Nakamura Ikeda, born in 1994.<br />
+                        I'm enthusiastic about Learning tools, skills and new technologies. Now my main focus is learn and earn new skills and Experience of frontend Developer.
+                    </p>
+                </div>
+
+                <div>
+                    <h3>Skills</h3>
                     <p>
                         JavaScript, React,  Html, CSS, Git, Angular, TypeScript
                     </p>
-                <h3>Learning</h3>
+                    <h3>Learning</h3>
                     <p>
                         Next.js, React-Native.
                     </p>
+                </div>
+
             </div>
             <div className='About-contact'>
                 <img src={profile} className='About-contact-img'></img>
 
                 <div className='About-contact-details'>
-                    <span>Twitter</span> 
-                    <span>github</span> 
-                    <span>Gmail</span> 
-                    <span>Twitter</span> 
+                    <span>Twitter</span>
+                    <span>github</span>
+                    <span>Gmail</span>
+                    <span>Twitter</span>
                 </div>
             </div>
         </section>
