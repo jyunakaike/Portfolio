514724e2650329c105ac8d9f126718d14078bb76
jyun <jyunakaike@gmail.com>
2022-03-09 12:53:41 -0400

    feat: create db.js

---
+++ src/Components/db/initialState.js
@@ -0,0 +1,47 @@
+export default {
+    portfolio:[
+        {
+            "id":1,
+            "image": "faq-accordion.jpg",
+            "title": "faq accordion card",
+            "languages":["html","css","responsive","js"],
+            "description": " build out this FAQ accordion card with optimal layout for the component depending on their device's screen size, interactive elements with hover states, this challenge is fron frontendmentor",
+            "page": "https://jyunakaike.github.io/faq-accordion-card-main/",
+        },
+        {
+            "id":2,
+            "image": "Tip-calculator-app.jpg",
+            "title": "Tip calculator app",
+            "languages":["html","css","js"],
+            "description": "build out this Tip calculator app User should able to Calculate the correct tip and total cost of the bill per person this challenge is fron frontendmentor",
+            "page": "https://jyunakaike.github.io/Tip-calculator-app-challenge-/",
+        },
+        {
+            "id":3,
+            "image": "Qr-component.jpg",
+            "title": "Qr code Component",
+            "languages":["html","css"],
+            "description": "build out this Qr Component A simple component of QR with Html and css. This challenge is fron frontendmentor",
+            "page": "https://jyunakaike.github.io/QRcodeComponent/",
+        },
+        {
+            "id":4,
+            "image": "Job-listings-with-filtering.jpg",
+            "title": "Job listings with filtering",
+            "languages":["html","css", "react", "responsive", "js" ],
+            "description": "build out this Job listings with filtering app with optimal layout for the component depending on their device's screen size, interactive elements with hover states. Users should be able to filter job listings based on the categories, this challenge is fron frontendmentor",
+            "page": "https://jyunakaike.github.io/StaticJobListingsMaster/",
+        },
+        {
+            "id":5,
+            "image": "E-commerce-product-page.jpg",
+            "title": "E-commerce product page",
+            "languages":["html","css", "Nextjs","react", "responsive", "js" ],
+            "description": "build out this E-commerce product page with filtering app with optimal layout for the component depending on their device's screen size. Users should be able to Open a lightbox gallery by clicking on the large product image,Switch the large product image by clicking on the small thumbnail images,Add items to the cart,View the cart and remove items from it; this challenge is fron frontendmentor",
+            "page": "https://e-commerce-product-page-challenge-hub.vercel.app/",
+        },
+        
+    ]
+
+
+}
