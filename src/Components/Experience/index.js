bb9872fb6c5961dc840911fad015d66b42b94a25
jyun <jyunakaike@gmail.com>
2022-03-14 11:21:54 -0400

    feat : using json portfolio list to bring info

--- src/Components/Experience/index.js
+++ src/Components/Experience/index.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 
 // component 
 import { Card } from './Cards';
 
+// db 
+import initialState from '../db/initialState'
+
 
 export const Experience = () => {
+
+  const listCards = initialState.portfolio
+
   return (
     <section className='Experience-container' id='Experience'>
       <div className='Experience-detail-container'>
@@ -14,7 +20,7 @@ export const Experience = () => {
 
           <div className='Experience-container-input' >
             <div className='Experience-input'>
-              <input  />
+              <input />
             </div>
 
             <div className='Experience-filters'>
@@ -24,63 +30,17 @@ export const Experience = () => {
               <span>Js</span>
               <span>css</span>
             </div>
-            
+
           </div>
         </div>
 
         <div className='Experience-card-container' >
           {/* cards */}
+          {
+            // listCards.map(listCar => <Card key={listCard.id} /> )
+            listCards.map(listCard =><Card key={listCard.id}  {...listCard} />)
+          }
 
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-
-          {/* <div className='Experience-card' >
-            <div className='Card-image'>Imagen</div>
-            <div className='Card-detail'>
-              <div className='Card-detail-title'>
-                <h3>Proyecto para mostrar numero 1 identificador de algo para que sea interesante</h3>
-              </div>
-
-              <div className='Card-detail-skills'>
-                <span>React</span>
-                <span>Html</span>
-                <span>JS</span>
-                <span>css</span>
-              </div>
-
-              <div className='Card-detail-description'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras maximus at arcu vestibulum mattis. Aliquam at faucibus mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec fringilla efficitur leo, eget porttitor nibh ultricies vitae. Nunc id sem vitae orci scelerisque rutrum. Fusce a dolor orci. Mauris malesuada vehicula magna, nec suscipit neque laoreet ut. In sit amet tincidunt felis, eu fringilla leo. Donec nec massa sem. Praesent tristique bibendum eros et vulputate. Suspendisse potenti. Praesent fringilla dictum ullamcorper. Donec tempor nisi id hendrerit imperdiet. Etiam elementum justo id sapien eleifend porttitor. Cras vel consectetur nisi. Pellentesque eu malesuada justo, ut laoreet orci.</p>
-              </div>
-            </div>
-
-          </div> */}
-
-
-          {/* <div className='Experience-card' >
-            <div className='Card-image'>Imagen</div>
-            <div className='Card-detail'>
-              <div className='Card-detail-title'>
-                <h3>Proyecto para mostrar numero 3</h3>
-              </div>
-
-              <div className='Card-detail-skills'>
-                <span>React</span>
-                <span>Html</span>
-                <span>JS</span>
-                <span>css</span>
-              </div>
-
-              <div className='Card-detail-description'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore </p>
-              </div>
-            </div>
-          </div>
-          <div className='Experience-card' >
-          </div>
-          <div className='Experience-card' >
-          </div> */}
         </div>
       </div>
     </section>
