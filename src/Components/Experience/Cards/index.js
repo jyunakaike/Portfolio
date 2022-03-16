fcc956dc88ec73101185c0004858a90a3eb537b3
jyun <jyunakaike@gmail.com>
2022-03-16 12:57:37 -0400

    feat: when click card image go to page

--- src/Components/Experience/Cards/index.js
+++ src/Components/Experience/Cards/index.js
@@ -3,12 +3,14 @@ import './styles.css'
 // import CardImage from '../../../assets/image/faq-accordion.jpg'
 
 
-export const Card = ({image , title, languages, description, page, filterEtiquete}) => {
+export const Card = ({ image, title, languages, description, page, filterEtiquete }) => {
     const imagen = "faq-accordion.jpg"
-    
+
     return (
         <div className='Experience-card' >
-            <img className='Card-image' src={ require(`../../../assets/image/${image}`  ) } />
+            <a href={page}>
+                <img className='Card-image' src={require(`../../../assets/image/${image}`)} ></img>
+            </a>
             <div className='Card-detail'>
                 <div className='Card-detail-title'>
                     <h3>{title}</h3>
@@ -16,7 +18,7 @@ export const Card = ({image , title, languages, description, page, filterEtiquet
 
                 <div className='Card-detail-skills'>
                     {
-                    languages.map(language => <span key={language}>{language}</span>)
+                        languages.map(language => <span key={language}>{language}</span>)
                     }
                 </div>
 
