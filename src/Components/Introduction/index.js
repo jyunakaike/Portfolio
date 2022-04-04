ce251804a947c4957e7eb3e6cd6df8d5325a4910
jyun <you@example.com>
2022-04-04 19:36:18 -0400

    feat: animation in contact detail

--- src/Components/Introduction/index.js
+++ src/Components/Introduction/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import './style.css';
+
+// import Jyunik from '../../assets/logos/jyunik.svg';
+import Jyunik from '../../assets/logos/jyunik.png';
 import icon from '../../assets/logos/icon.png';
+import './style.css';
+
+
 
 export const Introduction = () => {
   return (
@@ -10,9 +15,17 @@ export const Introduction = () => {
           <h1>Hi! I'm Jorge Jyun Nakamura</h1>
         </article>
         <article className='Intro-Detail'>
-          Aqui hay una imagen
+          {/* Aqui hay una imagen */}
+          {/* <Jyunik /> */}
+          {/* <img src={Jyunik} /> */}
+          {/* <img src={Jyunik} alt="React Logo" /> */}
+          <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 904 722">
+            <g id="Capa_1-2" data-name="Capa 1">
+              <image width="904" height="722" xlinkHref={Jyunik} />
+            </g>
+          </svg>
 
-          <img  src={icon} className="imageAni"/>
+          {/* <img  src={icon} className="imageAni"/> */}
         </article>
       </div>
     </section>
